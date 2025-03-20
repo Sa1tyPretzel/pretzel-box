6898e55f1b498becb7380f7558f04732538eebe9
Sa1tyPretzel <mithileshpawar1@gmail.com>
2025-03-21 01:26:17 +0530

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,12 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import { ClerkProvider } from '@clerk/nextjs'
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { PostHogProvider } from "./_providers/posthog-provider";
 export const metadata: Metadata = {
   title: "Pretzel Box",
   description: "shitty google drive",
-  icons: {
-    icon: '/icon.png',
-  },
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
@@ -18,7 +16,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
-        <body>{children}</body>
+        <body>
+          <PostHogProvider>{children}</PostHogProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
